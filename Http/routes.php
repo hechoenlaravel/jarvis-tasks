6861fdca11cb5e0de66374eace38de8e78075b6e
Jose Luis Fonseca <jose@ditecnologia.com>
2015-11-07 12:56:42 -0500

    auth Middleware

--- Http/routes.php
+++ Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'tasks', 'namespace' => 'Modules\Tasks\Http\Controllers'], function()
+Route::group(['prefix' => 'tasks', 'namespace' => 'Modules\Tasks\Http\Controllers', 'middleware' => ['auth']], function()
 {
 	Route::resource('/boards', 'BoardsController');
     Route::group(['prefix' => 'config', 'middleware' => ['acl:config-tasks']], function(){
