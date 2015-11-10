c2539498654f82cd6bd91d4359ce3fc6e9c3c2e1
Jose Luis Fonseca <jose@ditecnologia.com>
2015-11-10 08:20:25 -0500

    update users view

--- Resources/views/boards/show.blade.php
+++ Resources/views/boards/show.blade.php
@@ -7,7 +7,7 @@
 @endsection
 @section('content')
     <div class="row" ng-controller="BoardController">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="box box-info">
                 <div class="box-header with-border">
                     <h3 class="box-title">
@@ -18,11 +18,12 @@
                     </div>
                 </div>
                 <div class="box-body">
-
+                    <strong>Flujo:</strong> {{$board->flow->name}}<br />
+                    <strong>Descripción:</strong> {{$board->flow->description}}
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-6">
             <div class="box box-info">
                 <div class="box-header with-border">
                     <h3 class="box-title">
@@ -34,13 +35,13 @@
                 </div>
                 <div class="box-body no-padding">
                     <ul class="users-list clearfix">
-                        <li style="width: 50%">
+                        <li>
                             <img src="{{$board->user->getAvatarImageUrl()}}" alt="{{$board->user->name}}">
                             <a class="users-list-name" href="#">{{$board->user->name}}</a>
                             <span class="label label-success">Administrador</span>
                         </li>
                         @foreach($board->users as $user)
-                            <li style="width: 50%">
+                            <li>
                                 <img src="{{$user->user->getAvatarImageUrl()}}" alt="{{$user->user->name}}">
                                 <a class="users-list-name" href="#">{{$user->user->name}}</a>
                                 @if($user->can_assign)
